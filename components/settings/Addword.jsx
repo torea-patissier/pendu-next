bd66b244a0d99ec713acb28cd59052dbb27f787c
Torea Patissier <p.torea@icloud.com>
2023-03-09 15:56:48 +0100

    feat: - set up json server - POST route
    fix:
    - form onSubmit
    - clear input value onSubmit

--- components/settings/Addword.jsx
+++ components/settings/Addword.jsx
@@ -5,14 +5,43 @@ import {useState} from "react";
 export default function Addword() {
 
   const [word, setWord] = useState("");
+  const handleWord = (e) => {
+    setWord(e.target.value)
+  }
+
+  const PostWord = async (word) => {
+    try{
+        const response = await fetch("http://localhost:3004/words", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+            word: word
+            })
+        })
+      const data = await response.json();
+      console.log(data);
+      return data;
+    }catch (e) {
+      console.log(e)
+      throw new Error(e);
+    }
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    PostWord(word).then(r => console.log(r));
+    setWord("");
+  }
 
   return (
      <>
-       <form action="/send-data-here" className="w-1/2 p-5 bg-gray1 rounded-lg" method="post">
+       <form onSubmit={(e) => onSubmit(e)} className="w-1/2 p-5 bg-gray1 rounded-lg">
          <Inputs
             name="Add a new word"
             value={word}
-            onchange={e => setWord(e.target.value)}
+            onChange={handleWord}
          />
          <div className="flex justify-center">
            <SubmitButton/>
